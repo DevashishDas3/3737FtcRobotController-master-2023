ba7c9dc8d01e635e63d8a9847b22a4ce989e9407
DevashishDas3 <dedas2024@gmail.com>
2022-11-01 21:54:58 -0400

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotDrive_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotDrive_Linear.java
@@ -10,30 +10,7 @@ import com.qualcomm.robotcore.util.Range;
 public class BasicOpMode_Linear extends LinearOpMode {
 
     //    public void init(HardwareMap ahwMap) {
-    //        // Save reference to Hardware map
-    //        hwMap = ahwMap;
-    //
-    //        // Define and Initialize Motors
-    //        leftFront  = hwMap.get(DcMotor.class, "LF");
-    //        leftBack  = hwMap.get(DcMotor.class, "LB");
-    //        rightFront  = hwMap.get(DcMotor.class, "RF");
-    //        rightBack  = hwMap.get(DcMotor.class, "RB");
-    //
-    //        // Reversing left motors
-    //        rightFront.setDirection(DcMotor.Direction.FORWARD);
-    //        leftFront.setDirection(DcMotor.Direction.REVERSE);
-    //        rightBack.setDirection(DcMotor.Direction.FORWARD);
-    //        leftBack.setDirection(DcMotor.Direction.REVERSE);
-    //
-    //        // Set all motors to zero power'
-    //        setMotorPowers(0);
-    //
-    //        // Set all motors to run without encoders.
-    //        // May want to use RUN_USING_ENCODERS if encoders are installed.
-    //        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    //        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    //        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    //        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -69,28 +46,18 @@ public class BasicOpMode_Linear extends LinearOpMode {
             double LBPower;
             double RBPower;
 
-            // Setup a variable for each drive wheel to save power level for telemetry
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
             LFPower = Range.clip(drive + turn, -1.0, 1.0) ;
             RFPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+
 
             // Send calculated power to wheels
             leftFront.setPower(LFPower);
             rightDrive.setPower(RFPower);
 
-            // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.update();
